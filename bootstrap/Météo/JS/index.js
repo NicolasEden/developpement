33b65424b702e617991eb0a977bc527d671a9226
NefiteTifall <nefitetifall@gmail.com>
2020-01-09 00:16:27 +0100

    no message

--- bootstrap/Météo/JS/index.js
+++ bootstrap/Météo/JS/index.js
@@ -1,59 +1,130 @@
-var today = null;
-
-function act(){
+var city = null;
+$.ajax({
+    url: './City/city.json',
+    type: 'GET',
+    dataType: 'json',
+    async: false,
+    success : function(data) {
+        city = data;
+    }, error : function(req, err) {
+        alert: ("Request:"+ JSON.stringify(req));
+    }
+});
+function changeCity(event) {
+    if(event.code == "Enter"){
+        for (var i = 0; i < city.length; i++) {
+            if (city[i].name == document.getElementById('input').value) {
+                act(city[i].id);
+                return;
+            }
+        }
+    }
+}
+var core = document.body;
+function act(code){
+    document.body.innerHTML = '<div class="container"><div class="col col-hover" id="1"><input class="col-4" classtype="text" name="" value="" onkeypress="changeCity(event)" id="input"></div><div class="text-center"><div class="row" id="2"></div></div></div>'
+    var today = null;
+    var ville = null;
+    var jouractif = []
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/forecast?id='+code+'&APPID=fa956c3c094574e034c48dc970215933',
+        type: 'GET',
+        dataType: 'json',
+        async: false,
+        success : function(data) {
+            ville = data;
+        }, error : function(req, err) {
+            alert: ("Request:"+ JSON.stringify(req));
+        }
+    });
     newDiv = document.createElement("div");
+    newDiv.setAttribute('class', 'col-4')
     newName = document.createElement("p");
     newLocation = document.createElement("p");
-    newPopulation = document.createElement("p");
     newDiv.appendChild(newLocation);
     newDiv.appendChild(newName);
-    newDiv.appendChild(newPopulation);
 
 
     newName.innerHTML = '<p>Nom de la ville : <strong>'+ville.city.name+"</strong></p>";
     newLocation.innerHTML = '<p>Pays de la ville : <strong>'+ville.city.country+"</strong></p>";
-    newPopulation.innerHTML = '<p>Population de la Ville : <strong>'+ville.city.population+"</strong></p>";
-    document.getElementById('1').appendChild(newDiv);
+    console.log(ville);
+    document.getElementById('input').before(newDiv);
 
 
-    for (var i = 0; i < 8; i++) {
-        var todayTime = Date.now().toString()
-        if (timeConverter(todayTime.substr(0, 10)) == timeConverter(ville.list[i].dt)) today = "Aujourd'hui le";
-        else if (timeConverterNext(todayTime.substr(0, 10)) == timeConverter(ville.list[i].dt)) today = "Demain le";
-        else if (timeConverterNext2(todayTime.substr(0, 10)) == timeConverter(ville.list[i].dt)) today = "Après-demain le";
-        else today = "";
+    for (var i = 0; i < ville.list.length; i++) {
+        if (jouractif.indexOf(timeConverter(ville.list[i].dt)) == -1) {
+            var todayTime = Date.now().toString()
+            if (timeConverter(todayTime.substr(0, 10)) == timeConverter(ville.list[i].dt)) today = "Aujourd'hui le";
+            else if (timeConverterNext(todayTime.substr(0, 10)) == timeConverter(ville.list[i].dt)) today = "Demain le";
+            else if (timeConverterNext2(todayTime.substr(0, 10)) == timeConverter(ville.list[i].dt)) today = "Après-demain le";
+            else today = "";
 
-        newGenDiv = document.createElement("div");
-        newGenDiv.setAttribute('class', 'col-3')
-        document.getElementById('2').appendChild(newGenDiv);
-        newDiv = document.createElement("div");
-        newToday = document.createElement("p");
-        newTodayWeather = document.createElement("p");
-        newTodayCel = document.createElement("p");
-        newTodayWeatherImage = document.createElement("img");
-        newDiv.appendChild(newToday);
-        newDiv.appendChild(newTodayWeather);
-        newDiv.appendChild(newTodayCel);
-        newDiv.appendChild(newTodayWeatherImage);
-        var degKelDay = parseInt(ville.list[i].feels_like.day)
-        var degDay = (degKelDay - 273.15);
-        var degDayString = degDay.toString();
-        degDay = degDayString.substr(0, 4)
+            if (i == 24) {
+                newGenDiv = document.createElement("div");
+                newGenDiv.setAttribute('class', 'col-2');
+                newDiv = document.createElement("div");
+                document.getElementById('2').appendChild(newGenDiv);
+                newGenDiv.appendChild(newDiv);
+            }
+            newGenDiv = document.createElement("div");
+            newGenDiv.setAttribute('class', 'col-4 -hover')
+            document.getElementById('2').appendChild(newGenDiv);
+            newDiv = document.createElement("div");
+            newToday = document.createElement("p");
+            newTodayWeather = document.createElement("p");
+            newTodayCel = document.createElement("p");
+            newTodayWeatherImage = document.createElement("img");
+            newDiv.appendChild(newToday);
+            newDiv.appendChild(newTodayWeather);
+            newDiv.appendChild(newTodayCel);
+            newDiv.appendChild(newTodayWeatherImage);
+            var degKelDay = ville.list[i].main.temp
+            var degDay = (degKelDay - 273.15);
+            var degDayString = degDay.toString();
+            degDay = degDayString.substr(0, 4)
 
-        if (ville.list[i].weather[0].description == "light rain") newTodayWeather.innerHTML = 'Il y aura des nuages avec de la pluis'
-        if (ville.list[i].weather[0].description == "overcast clouds") newTodayWeather.innerHTML = 'Il y aura des gros nuages'
-        if (ville.list[i].weather[0].description == "broken clouds") newTodayWeather.innerHTML = 'Il y aura des gros nuages'
-        if (ville.list[i].weather[0].description == "scattered clouds") newTodayWeather.innerHTML = 'Il fera nuageux'
-        else newTodayWeather.innerHTM = "null"
-        console.log(i)
-        newTodayCel.innerHTML += ' '+degDay+" C°"
-        newTodayWeatherImage.setAttribute("src", "http://openweathermap.org/img/wn/"+ville.list[i].weather[0].icon+".png")
-        newToday.innerHTML = today+' <strong>'+timeConverter(ville.list[i].dt)+'</strong>'
-        newGenDiv.appendChild(newDiv);
+            if (ville.list[i].weather[0].description == "light rain") {
+                newTodayWeather.innerHTML = 'Pluis avec des nuages'
+                newTodayWeatherImage.setAttribute("src", "./Ressources/Icons/rainy-1.svg")
+            }
+            if (ville.list[i].weather[0].description == "overcast clouds") {
+                newTodayWeather.innerHTML = 'Fortement Nuageux'
+                newTodayWeatherImage.setAttribute("src", "./Ressources/Icons/cloudy.svg")
+            }
+            if (ville.list[i].weather[0].description == "broken clouds") {
+                newTodayWeather.innerHTML = 'Fortement Nuageux'
+                newTodayWeatherImage.setAttribute("src", "./Ressources/Icons/cloudy.svg")
+            }
+            if (ville.list[i].weather[0].description == "scattered clouds") {
+                newTodayWeather.innerHTML = 'Partiellement Nuageux'
+                newTodayWeatherImage.setAttribute("src", "./Ressources/Icons/cloudy-day-3.svg")
+            }
+            if (ville.list[i].weather[0].description == "few clouds") {
+                newTodayWeather.innerHTML = 'Partiellement Ensoleillé'
+                newTodayWeatherImage.setAttribute("src", "./Ressources/Icons/cloudy-day-3.svg")
+            }
+            if (ville.list[i].weather[0].description == "clear sky") {
+                newTodayWeather.innerHTML = 'Fortement Ensoleillé'
+                newTodayWeatherImage.setAttribute("src", "./Ressources/Icons/day.svg")
+            }
+            if (ville.list[i].weather[0].description == "light snow") {
+                newTodayWeather.innerHTML = 'Legère neige'
+                newTodayWeatherImage.setAttribute("src", "./Ressources/Icons/snowy-3.svg")
+            }
+            else newTodayWeather.innerHTM = "null"
+            newTodayCel.innerHTML += ' '+degDay+" C°"
+            newToday.innerHTML = today+' <strong>'+timeConverter(ville.list[i].dt)+'</strong>'
+            jouractif.push(timeConverter(ville.list[i].dt))
+            newGenDiv.appendChild(newDiv);
+        }
     }
 }
 
-
+function test() {
+    for (var i = 0; i < ville.list.length; i++) {
+        console.log(timeConverter(ville.list[i].dt));
+    }
+}
 
 
 
